f0e813fe5940cee458b20d60aba88cea14425d27
Skunkynator <43096109+Skunkynator@users.noreply.github.com>
2021-08-27 20:36:24 +0200

    Event Collision: Add Area Support

--- gameplay/event_collision.gd
+++ gameplay/event_collision.gd
@@ -2,6 +2,16 @@ extends CollisionObject
 class_name EventCollision
 
 
+func _ready() -> void:
+	if has_signal("body_entered"):
+		connect("body_entered",self,"_on_collision_body_entered")
+
+
 func _on_player_entered(player: Player) -> void:
 	print_debug("uwu")
 	pass
+
+
+func _on_collision_body_entered(body):
+	if body is Player:
+		_on_player_entered(body)
