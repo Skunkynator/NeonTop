5338e345bcce29fcf1d56674189c1b28f221201e
Skunkynator <43096109+Skunkynator@users.noreply.github.com>
2021-09-17 22:20:35 +0200

    Level_controller: add load_level function

--- gameplay/level_controller.gd
+++ gameplay/level_controller.gd
@@ -8,6 +8,19 @@ var start_transform : Transform
 var reset_transform : Transform
 
 
+func load_level(path: String) -> void:
+	var new_level : PackedScene = load(path)
+	if new_level == null:
+		return
+	var root_type := new_level.get_state().get_node_type(0)
+	if root_type != "Level":
+		return
+	_ExtendedFunctions.delete_children(self)
+	var level : Level = new_level.instance() 
+	add_child(level)
+	start_transform = level.get_start_transform()
+
+
 func reset_level() -> void:
 	object_timer = reset_timer
 
@@ -15,6 +28,7 @@ func reset_level() -> void:
 func restart_level() -> void:
 	object_timer = 0
 	reset_timer = object_timer
+	reset_transform = start_transform
 
 
 func get_reset_transform() -> Transform:
