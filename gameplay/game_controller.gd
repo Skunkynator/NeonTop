a7066748fed93c97a670863880fce58a9ff725d1
Skunkynator <43096109+Skunkynator@users.noreply.github.com>
2021-12-30 21:22:44 +0100

    game_controller: add set_main_player

--- gameplay/game_controller.gd
+++ gameplay/game_controller.gd
@@ -1,7 +1,18 @@
 extends Node
 
 
-var main_player : Player
+var main_player : Player setget set_main_player
+
+
+func set_main_player(player: Player):
+	if not player:
+		return
+	if main_player == player:
+		return
+	if main_player:
+		main_player.queue_free()
+	main_player = player
+	add_child(main_player)
 
 
 func reset_level() -> void:
